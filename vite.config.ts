a530e3017f3f63eaa131f65f8901d97bd83807bf
William Domingues <williamcdzx88@gmail.com>
2025-12-30 08:52:06 -0300

    Atualiza conteúdo do portfólio

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/MeuPortifolioWilliamDomingues/",
+  base: mode === "development" ? "/" : "/MeuPortifolioWilliamDomingues/",
   server: {
     host: "::",
     port: 8080,
